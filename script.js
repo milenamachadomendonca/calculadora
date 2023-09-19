9fcaa56c22cf4e5887f3c85d95486ef8f5abc90c
milenamachadomendonca <137801509+milenamachadomendonca@users.noreply.github.com>
2023-09-19 11:46:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+calcule.addEventListener('click', function(){
+
+ 
+ var a = valorA.value; 
+ var b = valorB.value;
+ var op = operacao.value;
+
+a=parseInt(a);
+b=parseInt(b);
+
+var result = a + b;
+var result = a - b;
+
+ alert(result);
+
+ });
